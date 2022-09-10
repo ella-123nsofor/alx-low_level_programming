17ef12160c2cf7010d5528dd35758a66f2ad3188
ella-123nsofor <elladavid148@gmail.com>
2022-09-09 21:20:43 -0700

    finally, done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Prints numbers 00 to 99.
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
+
 int main(void)
 {
-	int i, e, g, h, op1, op2;
+	int p, q;
 
-	i = e = g = h = 48;
-	while (h < 58)
+	for (p = 0; p<= 98; p++)
 	{
-		g = 48;
-		while (g < 58)
+		for (q = p + 1; q <= 99; q++)
 		{
-			e = 48;
-			while (e < 58)
-			{
-				i = 48;
-				while (i < 58)
-				{
-					op1 = (h * 10) + g;
-					op2 = (e * 10) + i;
-					if (op1 < op2)
-					{
-						putchar(h);
-						putchar(g);
-						putchar(' ');
-						putchar(e);
-						putchar(i);
-						if (h == 57 && g == 56 && e == 57 && i == 57)
-							break;
-						putchar(',');
-						putchar(' ');
-					}
-					i++;
-				}
-				e++;
-			}
-			g++;
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
+
+			if (p == 98 && q ==99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
-		h++;
 	}
+
 	putchar('\n');
+
 	return (0);
 }
