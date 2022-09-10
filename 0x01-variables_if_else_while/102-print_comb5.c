a817bb651c02f84616e8456330e4ce3c7221c785
ella-123nsofor <elladavid148@gmail.com>
2022-09-09 21:40:30 -0700

    finished

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	int p, q;
 
-	for (p = 0; p<= 98; p++)
+	for (p = 0; p < = 98; p++)
 	{
-		for (q = p + 1; q <= 99; q++)
+		for (q = p + 1; q < = 99; q++)
 		{
 			putchar((p / 10) + '0');
 			putchar((p % 10) + '0');
@@ -22,7 +22,7 @@ int main(void)
 			putchar((q / 10) + '0');
 			putchar((q % 10) + '0');
 
-			if (p == 98 && q ==99)
+			if (p == 98 && q == 99)
 				continue;
 
 			putchar(',');
