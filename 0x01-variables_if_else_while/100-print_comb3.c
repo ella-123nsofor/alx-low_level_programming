d63f7126fa664d7eb29e58c8ade50ab1bfa5d14a
ella-123nsofor <elladavid148@gmail.com>
2022-09-09 19:49:51 -0700

    101-done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 99.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	2 = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	e++;
+}
+putchar('\n');
+return (0);
+}
