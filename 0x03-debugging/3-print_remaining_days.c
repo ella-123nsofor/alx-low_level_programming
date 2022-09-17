fac344efff38ac30b5a0d5c43e021acd5fa52df5
ella-123nsofor <elladavid148@gmail.com>
2022-09-16 20:41:36 -0700

    herd

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,13 +14,10 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
-		if (month >= 2 && day >= 60)
-		{
+		if (month > 2)
 			day++;
-		}
-
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 306 - day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
@@ -31,7 +28,7 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 306 - day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
